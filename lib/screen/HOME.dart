008e79b9bcd57e89419580c1ab605464677f7070
Salsabillasyazwani <syazwanibila@gmail.com>
2025-11-07 07:29:42 +0700

    Create HOME widget with counter and provider integration

---
+++ lib/screen/HOME.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../riverpod/hello_riverpod.dart';
+
+class HOME extends HookConsumerWidget {
+  const HOME({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = useState(0);
+    final hello = ref.watch(helloProvider);
+    return Scaffold(
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            return Text('$hello ${counter.value}');
+          },
+        ),
+      ),
+    );
+  }
+}
